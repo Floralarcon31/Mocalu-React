22facc0b8e0b96a24e2db1568d8a861995b458af
floralarcon31 <floralarcon31@gmail.com>
2022-02-09 22:25:25 -0300

    desafio item collection / firestore conectado a itemlist e itemDetail container

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import {  useParams} from 'react-router-dom';
 import ItemDetail from "./ItemDetail";
+import { getFirestore } from '../../firebase/firebase'
 
 export default function ItemDetailContainer() {
 
@@ -8,76 +9,37 @@ export default function ItemDetailContainer() {
 
   const [producto, setProducto] = useState({});
 
-  useEffect(()=>{
+    
+    
+  useEffect(() => {
+    //base de datos
+    const db = getFirestore();
+    //traer los items - nombre de la coleccion que esta en firebase
+    const itemCollection = db.collection("items");
 
-    setTimeout(() => {
+    //id del producto - trae uno
+    const miItem = itemCollection.doc(itemid);
 
-      const listadoProductos = ([
-            {
-                "id":1,
-                "name":"Vestido Rubens",
-                "categoria":"vestido",
-                "descripcion":"vestido rosado",
-                "precio":12950,
-                "stock":10,
-                "img":"https://cardon.com.ar/wp-content/uploads/2021/10/AC-234402004-59-725.jpg"
-            },
-            {
-                "id":2,
-                "name":"Vestido Chicoana",
-                "categoria":"vestido",
-                "descripcion":"vestido rojo",
-                "precio":16170,
-                "stock":12,
-                "img":"https://cardon.com.ar/wp-content/uploads/2021/09/AC-234116007-50-235.jpg"
-            },
-            {
-                "id":3,
-                "name":"Vestido de las Vueltas Estampado",
-                "categoria":"vestido",
-                "descripcion":"vestido largo azul y amarillo",
-                "precio":13436,
-                "stock":8,
-                "img":"https://cardon.com.ar/wp-content/uploads/2021/11/AC-234402005-30-738.jpg"
-            },
-            {
-                "id":4,
-                "name":"Vestidos Rio Iruya",
-                "categoria":"vestido",
-                "descripcion":"vestido blanco corto",
-                "precio":5120,
-                "stock":11,
-                "img":"https://cardon.com.ar/wp-content/uploads/2021/08/CD-234215013-01-795.jpg"
-            },
-            {
-                "id":5,
-                "name":"Camisa Lourdes",
-                "categoria":"camisa",
-                "descripcion":"camisa azul con detalles de colores",
-                "precio":8890,
-                "stock":5,
-                "img":"https://cardon.com.ar/wp-content/uploads/2021/12/SI-240188190-30-904.jpg"
-            },
-            {
-                "id":6,
-                "name":"Camisa Rio Deseado Lino",
-                "categoria":"camisa",
-                "descripcion":"camisa blanca",
-                "precio":11880,
-                "stock":3,
-                "img":"https://cardon.com.ar/wp-content/uploads/2021/10/SI-241640012-01-2244.jpg"
-            },
-        ]);
+    miItem.get()    
+      .then((doc) => {
+        /*
+        console.log(doc.data());
+        console.log(doc.id);
+        */
+       //controlas existes si llegao no el documento
+        if (!doc.exists) {
+          console.log('no existe ese documento');
+          return
+        }
+        //si llega prducto
+        console.log('item found');
+        setProducto({ id: doc.id, ...doc.data() });
 
-        const Producto = listadoProductos.filter(item => item.id == itemid) ; //CAMBIAR A FIND(?)
-
-        let myProduct = Producto[0];
-
-        setProducto(myProduct);
-
-    }, 2000);
-
-  }, [itemid] );
+      })
+      .catch((err)=>{
+        console.log(err);
+      })
+  }, [])
 
   return (
    
--- src/components/itemListContainer/itemListContainer.jsx
+++ src/components/itemListContainer/itemListContainer.jsx
@@ -1,98 +1,46 @@
 import React, { useState, useEffect } from "react"
 import ItemList from "./ItemList"
 import {  useParams} from 'react-router-dom';
+import { getFirestore } from '../../firebase/firebase'
 
-const ItemListContainer = (props) => {
-    
-    const{categoriaId} = useParams();
-    
+const ItemListContainer = (props) => {     
+    const{categoriaId} = useParams();  
     const [arrayDeProductos, setArrayDeProductos] = useState([]);
+    useEffect(() => {
+      const db = getFirestore();  
+      let itemCollection ;
 
-    const Productos = new Promise ((resolve,reject) =>{
-        setTimeout( () => {
-            resolve([
-                {
-                    "id":1,
-                    "name":"Vestido Rubens",
-                    "categoria":"vestido",
-                    "descripcion":"vestido rosado",
-                    "precio":12950,
-                    "stock":10,
-                    "img":"https://cardon.com.ar/wp-content/uploads/2021/10/AC-234402004-59-725.jpg"
-                },
-                {
-                    "id":2,
-                    "name":"Vestido Chicoana",
-                    "categoria":"vestido",
-                    "descripcion":"vestido rojo",
-                    "precio":16170,
-                    "stock":12,
-                    "img":"https://cardon.com.ar/wp-content/uploads/2021/09/AC-234116007-50-235.jpg"
-                },
-                {
-                    "id":3,
-                    "name":"Vestido de las Vueltas Estampado",
-                    "categoria":"vestido",
-                    "descripcion":"vestido largo azul y amarillo",
-                    "precio":13436,
-                    "stock":8,
-                    "img":"https://cardon.com.ar/wp-content/uploads/2021/11/AC-234402005-30-738.jpg"
-                },
-                {
-                    "id":4,
-                    "name":"Vestidos Rio Iruya",
-                    "categoria":"vestido",
-                    "descripcion":"vestido blanco corto",
-                    "precio":5120,
-                    "stock":11,
-                    "img":"https://cardon.com.ar/wp-content/uploads/2021/08/CD-234215013-01-795.jpg"
-                },
-                {
-                    "id":5,
-                    "name":"Camisa Lourdes",
-                    "categoria":"camisa",
-                    "descripcion":"camisa azul con detalles de colores",
-                    "precio":8890,
-                    "stock":5,
-                    "img":"https://cardon.com.ar/wp-content/uploads/2021/12/SI-240188190-30-904.jpg"
-                },
-                {
-                    "id":6,
-                    "name":"Camisa Rio Deseado Lino",
-                    "categoria":"camisa",
-                    "descripcion":"camisa blanca",
-                    "precio":11880,
-                    "stock":3,
-                    "img":"https://cardon.com.ar/wp-content/uploads/2021/10/SI-241640012-01-2244.jpg"
-                },
-            ]);
-        }, 2000)
-    });
+      if (categoriaId != undefined) {
+       itemCollection= db.collection("items").where("categoria", "==", categoriaId)
+      } else {
+       itemCollection = db.collection("items")
+      }
 
-    useEffect(()=>{
-        Productos
-            .then(res => { 
-
-                categoriaId
-
-                ? setArrayDeProductos(
-                    res.filter ((prod)=> prod.categoria === categoriaId )
-                )
-                :
-                setArrayDeProductos(res)
-                
-            })
-    }, [categoriaId])
-
-   
+      itemCollection.get()
+          .then((querySnapShot) => {   
+            if (querySnapShot.size == 0) {
+              console.log('No hay documentos con en ese query');
+              return
+            }
+            console.log('hay documentos');   
+            //console.log(querySnapShot.docs);
+            setArrayDeProductos(querySnapShot.docs.map((doc)=> {
+                return { id: doc.id, ...doc.data() }
+            }
+            ));
+          })
+          .catch((err)=>{
+            console.log(err);
+        })
+  }, [categoriaId])
     return (
         <>
             <p className="itemListContainerP">{props.greeting}</p>
-
+ 
             <p> Estoy en la categoria{categoriaId || "principal" }</p>
 
             <ItemList list={arrayDeProductos} />
-           
+
         </>
         
     )
